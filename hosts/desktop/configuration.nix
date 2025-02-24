19c02df5f471b297512db6c63c47283e85f1664c
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-02-23 21:20:39 -0600

    removed commented proxy

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -19,11 +19,6 @@
   boot.initrd.kernelModules = [ "amdgpu "];
 
   networking.hostName = "rowan-desktop"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
