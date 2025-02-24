7c604f3f54d9d7d9a19b216ee26f3d95bb93a404
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-02-24 00:31:13 -0600

    add dockerO

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -80,10 +80,6 @@
   # Bluetooth :)
   # services.blueman.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.rowan = {
     isNormalUser = true;
     description = "Rowan";
@@ -150,16 +146,7 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  virtualisation.docker.enable = true;  
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
