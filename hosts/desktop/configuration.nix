8a897e5e67cbea9f53964c9a73777836f56ad580
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-03-09 18:12:19 -0500

    gnugp agent for pass

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -156,10 +156,11 @@ let unstablepkgs = inputs.nixpkgsUnstable.legacyPackages.${pkgs.system}; in
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryFlavor = "curses";
+  };
 
   virtualisation.docker.enable = true;  
 
