5c368f868cd4ee383283953bcbf6c0f92ef9f082
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-03-10 19:49:05 -0500

    500 error on calva, time to wait

--- nixosModules/myVSCodium.nix
+++ nixosModules/myVSCodium.nix
@@ -13,11 +13,12 @@
         ms-azuretools.vscode-docker
         ms-vscode-remote.remote-ssh
         haskell.haskell
+        vscodevim.vim
       ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         {
           name = "calva";
           publisher = "betterthantomorrow";
-          version = "v2.0.486";
+          version = "v2.0.490";
           sha256 = "sha256-pL+OgJvIK5eqE5Kr/wDeJ+2BGUT9Uj42coWSHtbPolk=";
         }
       ];
