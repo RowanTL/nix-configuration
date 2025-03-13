b553de661a4c44038934242d0ffdf44ad9b58dd4
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-03-13 03:55:26 -0500

    disable sway here

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -71,10 +71,10 @@
     # EDITOR = "emacs";
   };
 
-  xdg.configFile."sway/config".source = ./configs/sway/config;
-  wayland.windowManager.sway = {
-    enable = true;
-  };
+  # xdg.configFile."sway/config".source = ./configs/sway/config;
+  # wayland.windowManager.sway = {
+    # enable = true;
+  # };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
