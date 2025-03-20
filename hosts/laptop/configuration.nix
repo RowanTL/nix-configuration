a44ac48535fa6a39b94d38752b0909fe7a9aab15
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-03-20 13:37:46 -0500

    powersave config

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -28,7 +28,10 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    wifi.powersave = true;
+  };
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
