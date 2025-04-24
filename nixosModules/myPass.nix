72b0254b80dbad7de8ad9f8ef28540ef4c6d361f
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-04-24 14:13:11 -0500

    discourse post

--- nixosModules/myPass.nix
+++ nixosModules/myPass.nix
@@ -8,5 +8,8 @@
       pass-otp
     ]))
   ];
-  environment.etc."profile.d/pass_completions.sh".source = "${pkgs.pass-wayland.extensions.pass-otp}/share/bash-completion/completions/pass-otp";
+  # https://discourse.nixos.org/t/finally-got-pass-otp-bash-completions/48627
+  programs.bash.interactiveShellInit = ''
+    . ${pkgs.pass.extensions.pass-otp}/share/bash-completion/completions/pass-otp
+  '';
 }
