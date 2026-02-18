40bdbdb79a70046a60133d2c74092ea1176d1e4e
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-02-18 16:43:32 -0600

    remove networking.wireless.enable = true

--- hosts/rowan-laptop-test/configuration.nix
+++ hosts/rowan-laptop-test/configuration.nix
@@ -15,7 +15,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "rowan-laptop-test"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
