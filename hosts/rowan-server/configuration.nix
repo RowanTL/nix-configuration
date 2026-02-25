2a76e360f5abaa8d04e4c59d953f007676de4233
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-02-24 22:10:08 -0600

    add this incase want to install cuda later

--- hosts/rowan-server/configuration.nix
+++ hosts/rowan-server/configuration.nix
@@ -128,6 +128,12 @@
 
   services.xserver.videoDrivers = ["nvidia"];
 
+  # settings for CUDA
+  nix.settings = {
+    substituters = [ "https://cache.nixos-cuda.org" ];
+    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
