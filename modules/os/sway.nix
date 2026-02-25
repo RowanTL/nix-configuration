5c4fa73516a96f7e13ba2aad22cd5d8455e29365
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-02-24 20:47:59 -0600

    fix condition unmet WAYLAND_DISPLAY was not met

--- modules/os/sway.nix
+++ modules/os/sway.nix
@@ -11,5 +11,9 @@
     services.gnome.gnome-keyring.enable = true;
     programs.light.enable = true;
     programs.sway.enable = true;
+
+    # Fix swayidle, kanshi, and others not finding WAYLAND_DISPLAY
+    # https://github.com/NixOS/nixpkgs/issues/407700
+    systemd.services.display-manager.environment.XDG_CURRENT_DESKTOP = "X-NIXOS-SYSTEMD-AWARE";
   };
 }
