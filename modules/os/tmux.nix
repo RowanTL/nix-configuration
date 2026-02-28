f4eb0986b599b0e5a3cec7aa1c7433daa749f636
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-02-27 18:02:42 -0600

    basic tmux config

---
+++ modules/os/tmux.nix
@@ -0,0 +1,20 @@
+{ lib, config, ... }:
+
+{
+  options = {
+    tmux.enable
+      = lib.mkEnableOption "enable custom tmux config";
+  };
+
+  config = lib.mkIf config.tmux.enable {
+    programs.tmux = {
+      enable = true;
+      clock24 = true;
+      historyLimit = 10000;
+      escapeTime = 5;
+      extraConfig = ''
+        set -g default-terminal "tmux-256color"
+      '';
+    }; 
+  }
+}
