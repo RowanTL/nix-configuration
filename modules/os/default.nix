54f2fa6291216420fc639a41b501e520b0beb790
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-03-04 10:17:33 -0600

    nix-community cachix for package building speed up

--- modules/os/default.nix
+++ modules/os/default.nix
@@ -7,6 +7,15 @@
   ];
 
   config = {
+    # cache to speed up package building
+    nix.settings.substituters = [
+      "https://nix-community.cachix.org"
+    ];
+
+    nix.settings.trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+
     environment.systemPackages = with pkgs; [
       pavucontrol
       ncdu
