5d2b5944b1ca97de6f4ff45e125911a5466161b8
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-03-06 15:30:20 -0600

    move config over from default configs

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -24,5 +24,17 @@
           IdentityFile ~/.ssh/roebox
       ";
     };
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
   };
 }
