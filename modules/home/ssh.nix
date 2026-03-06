5c29ba4952d7f4305cf426178e0fe1f2ae2495d7
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-03-06 15:31:39 -0600

    place matchBlocks inside correct curly brackets

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -23,18 +23,18 @@
           Hostname 192.168.1.139
           IdentityFile ~/.ssh/roebox
       ";
-    };
-    matchBlocks."*" = {
-      forwardAgent = false;
-      addKeysToAgent = "no";
-      compression = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = false;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
-      controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
     };
   };
 }
