63cdb87d5568d249e432b44a4898963485bbb783
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-03-06 14:50:13 -0600

    set volume gain/loss to 1%

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -95,8 +95,8 @@
             # brightness and volume keybindings
             "XF86MonBrightnessDown" = "exec light -U 10";
             "XF86MonBrightnessUp" = "exec light -A 10";
-            "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
-            "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
+            "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +1%";
+            "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -1%";
             "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
           }
         ];
