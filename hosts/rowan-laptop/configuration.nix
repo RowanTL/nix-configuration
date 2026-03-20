8fce946d8abbc7dbc0cf669cb8ff6a27c30d27aa
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-03-20 12:52:36 -0500

    remove user=rowan from seat group

--- hosts/rowan-laptop/configuration.nix
+++ hosts/rowan-laptop/configuration.nix
@@ -66,7 +66,7 @@
   users.users.rowan = {
     isNormalUser = true;
     description = "rowan";
-    extraGroups = [ "networkmanager" "wheel" "video" "seat" ];
+    extraGroups = [ "networkmanager" "wheel" "video" ];
     # packages = with pkgs; [];
   };
 
