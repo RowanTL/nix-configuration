2137d5cca80a6f63b2a40c21e00dc974c7c28ccd
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-04-02 14:57:37 -0500

    270 needs to be a string, not an int

--- hosts/rowan-desktop/home.nix
+++ hosts/rowan-desktop/home.nix
@@ -82,7 +82,7 @@
               status = "enable";
               mode = "1366x768";
               position = "-768,0";
-              transform = 270;
+              transform = "270";
             }
             {
               criteria = "HDMI-A-1";
@@ -90,7 +90,7 @@
               status = "enable";
               mode = "1920x1080@60Hz";
               position = "1920,0";
-              transform = 270;
+              transform = "270";
             }
           ];
         };
