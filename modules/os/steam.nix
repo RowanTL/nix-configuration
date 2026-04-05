970ed01d373deaab1be0281ab617acb933f86fa3
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-04-05 17:59:35 -0500

    add enable option to gamescopeSession

--- modules/os/steam.nix
+++ modules/os/steam.nix
@@ -3,13 +3,13 @@
 {
   options = {
     steam.enable
-      = lib.mkEnableOption "enable custom tmux config";
+      = lib.mkEnableOption "enable custom steam config";
   };
 
   config = lib.mkIf config.steam.enable {
     programs.steam = {
       enable = true;
-      gamescopeSession = true;
+      gamescopeSession.enable = true;
     }; 
     programs.gamemode.enable = true;
   };
