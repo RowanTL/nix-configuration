65a9845e6c3f98d4d1cc75ea2a3fbb8ecf6748b2
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-04-22 10:41:20 -0500

    try sourcing from this directory instead

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -122,7 +122,7 @@
       export UV_PYTHON_DOWNLOADS=never
 
       # This line could or could not run. It doesn't matter
-      source ${pkgs.pass.extensions.pass-otp}/share/bash-completion/completions/pass-otp
+      source /run/current-system/sw/share/bash-completion/completions/pass-otp
     '';
   };
 
