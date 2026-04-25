9bff726437cfd7423c782089606d3f8258006128
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-04-25 17:31:41 -0500

    add pass otp completion script to nushell

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -1,5 +1,13 @@
 { lib, config, pkgs, ... }:
 
+let
+  extraPassCompletions = pkgs.fetchFromGitHub {
+    owner = "RowanTL";
+    repo = "nushell_pass_otp_completions";
+    rev = "1579c700bca760e7ce82ba9cc9a6942a01987c51";
+    hash = "sha256-swpJCjdjn0wLvFyscvwM+t+UX2td9MtnMK60mmA5lrE=";
+  };
+in
 {
   options = {
     home-shell.enable
@@ -21,6 +29,9 @@
         enable = true;
         # gonna leave config in their respective .nu files
         configFile.source = ../non_nix/nu/config.nu;
+        extraConfig = ''
+          source ${extraPassCompletions}/pass_extensions_completions.nu
+        '';
       };
       carapace = {
         enable = true;
