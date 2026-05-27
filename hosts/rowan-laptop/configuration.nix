e6092bb2ff7b5b92b519201c409f987e0cef05b2
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-05-27 14:57:44 -0700

    uhk agent

--- hosts/rowan-laptop/configuration.nix
+++ hosts/rowan-laptop/configuration.nix
@@ -74,10 +74,11 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-  # ];
+    uhk-agent
+  ];
 
   # intel integrated settings
   services.xserver.videoDrivers = [ "modesetting" ];
