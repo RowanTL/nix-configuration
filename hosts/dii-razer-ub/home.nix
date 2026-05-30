4af5d042f57291561c6cf05daa6891c1a8d2b44d
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-05-29 17:30:46 -0700

    beef up basic home file

--- hosts/dii-razer-ub/home.nix
+++ hosts/dii-razer-ub/home.nix
@@ -1,10 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  imports = [
+    ./../../modules/home
+    ./../../modules/home/helix.nix
+    ./../../modules/home/git.nix
+    ./../../modules/home/ssh.nix
+    ./../../modules/home/sway.nix
+    ./../../modules/home/zed.nix
+  ];
   home.username = "rtorblane";
   home.homeDirectory = "/home/rtorblane";
+
+  home-sway.enable = true;
+  home-zed.enable = true;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -18,27 +27,10 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
+  # home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -49,7 +41,7 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  };
+  # };
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
@@ -67,9 +59,9 @@
   #
   #  /etc/profiles/per-user/rtorblane/etc/profile.d/hm-session-vars.sh
   #
-  home.sessionVariables = {
+  # home.sessionVariables = {
     # EDITOR = "emacs";
-  };
+  # };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
