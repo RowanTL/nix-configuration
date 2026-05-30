9add4b4fad4e5ec293f11a41e0d0670005f982eb
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-05-29 18:47:20 -0700

    setup graphics on nix

--- hosts/dii-razer-ub/home.nix
+++ hosts/dii-razer-ub/home.nix
@@ -3,6 +3,17 @@
 {
   nix.package = pkgs.nix;
 
+  targets.genericLinux = {
+    enable = true;
+    gpu.nvidia = {
+      enable = true;
+      version = "595.71.05";
+      sha256 = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
+    };
+  };
+  # gpu offloading
+  # targets.genericLinux.nixGL.prime.installScript = "nvidia";
+
   imports = [
     ./../../modules/home
     ./../../modules/home/helix.nix
