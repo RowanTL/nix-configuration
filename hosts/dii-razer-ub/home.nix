299231fa74a2c62ac8e31a700a4aa0d3c2a806f1
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-06-17 11:20:23 -0700

    comment out service and use pkgs.ollama-cuda

--- hosts/dii-razer-ub/home.nix
+++ hosts/dii-razer-ub/home.nix
@@ -44,12 +44,13 @@
   # environment.
   home.packages = [
     pkgs.blender
+    pkgs.ollama-cuda
   ];
 
-  services.ollama = {
-    enable = true;
-    package = pkgs.ollama-cuda;
-  };
+  # services.ollama = {
+  #   enable = true;
+  #   package = pkgs.ollama-cuda;
+  # };
 
   # home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
