126d0e8eaed8c63e719e2394762fa1dc52337818
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-06-18 08:30:13 -0700

    change model tokens and max_output_tokens

--- modules/home/zed.nix
+++ modules/home/zed.nix
@@ -50,42 +50,46 @@ in
               {
                 name = "gemini-2.5-flash";
                 display_name = "Gemini 2.5 Flash";
-                max_tokens = 8192;
+                max_tokens = 1048576;
+                max_output_tokens = 65536;
               }
               {
                 name = "gemini-2.5-flash-lite";
                 display_name = "Gemini 2.5 Flash-Lite";
-                max_tokens = 8192;
-              }
+                max_tokens = 1048576;
+                max_output_tokens = 65536;             }
               {
                 name = "gemini-2.5-pro";
                 display_name = "Gemini 2.5 Pro";
-                max_tokens = 8192;
+                max_tokens = 1048576;
+                max_output_tokens = 65536;
               }
               {
                 name = "gemini-3-flash";
                 display_name = "Gemini 3 Flash-Lite";
-                max_tokens = 8192;
-              }
+                max_tokens = 1048576;
+                max_output_tokens = 65536;             }
               {
                 name = "gemini-3.1-flash-lite";
                 display_name = "Gemini 3.1 Flash-Lite";
-                max_tokens = 8192;
+                max_tokens = 1048576;
+                max_output_tokens = 65536;
               }
               {
                 name = "gemini-3.1-pro";
                 display_name = "Gemini 3.1 Pro";
-                max_tokens = 8192;
-              }
+                max_tokens = 1048576;
+                max_output_tokens = 65536;             }
               {
                 name = "gemini-3.5-flash";
                 display_name = "Gemini 3.5 Flash";
-                max_tokens = 8192;
-              }
+                max_tokens = 1048576;
+                max_output_tokens = 65536;              }
               {
-                name = "gemini-4-31b-it";
+                name = "gemma-4-31b-it";
                 display_name = "Gemma 4 31B";
-                max_tokens = 8192;
+                max_tokens = 256000;
+                max_output_tokens = 8192;
               }
             ];
           };
