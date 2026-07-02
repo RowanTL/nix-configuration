3dc6cf3d9a111dec090f8f47e7dfc42ed599bc5e
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-07-02 10:07:46 -0700

    add xdg-desktop-portal-wlr extraPortals in sway's os config

--- modules/os/sway.nix
+++ modules/os/sway.nix
@@ -24,9 +24,9 @@
     xdg.portal = {
       enable = true;
       wlr.enable = true;
-      # extraPortals = [
-      #   pkgs.xdg-desktop-portal-gtk
-      # ];
+      extraPortals = [
+        pkgs.xdg-desktop-portal-wlr
+      ];
     };
 
     services.pipewire = {
