265eb6c9c10d0dfc573473803e4f79053886a907
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-07-05 14:18:23 -0700

    add proper sha to vscode lean extension

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -9,14 +9,14 @@
   config = lib.mkIf config.home-vscode.enable {
     programs.vscode = {
       enable = true;
-      extensions = with pkgs.vscode-extensions; [
+      profiles.default.extensions = with pkgs.vscode-extensions; [
         # Another extension here
       ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         {
           name = "lean4";
           publisher = "leanprover";
           version = "0.0.238";
-          sha256 = "";
+          sha256 = "IhEmBDy+j+Ha5WHx+L9zLfd3e7jGb3cZSLAUZv3+RsE";
         }
       ];
     };
