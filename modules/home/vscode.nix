7ec1c068968b407b33f18948949d57394c66fbbe
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-07-05 14:15:17 -0700

    add vscode with lean extension to home-manager

---
+++ modules/home/vscode.nix
@@ -0,0 +1,24 @@
+{ lib, config, pkgs, ... }:
+
+{
+  options = {
+    home-vscode.enable
+      = lib.mkEnableOption "enable custom vscode";  
+  };
+  
+  config = lib.mkIf config.home-vscode.enable {
+    programs.vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        # Another extension here
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "lean4";
+          publisher = "leanprover";
+          version = "0.0.238";
+          sha256 = "";
+        }
+      ];
+    };
+  };
+}
