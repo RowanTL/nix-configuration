73cd5a6533f39387c8fe0b326e02ebd8a687a868
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-07-07 18:49:52 -0700

    Replace match blocks with proper settings config in home ssh.nix. Thx claude.

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -27,17 +27,17 @@
           Hostname 192.168.1.139
           IdentityFile ~/.ssh/roebox
       ";
-      matchBlocks."*" = {
-        forwardAgent = false;
-        addKeysToAgent = "no";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
+      settings."*" = {
+        ForwardAgent = false;
+        AddKeysToAgent = "no";
+        Compression = false;
+        ServerAliveInterval = 0;
+        ServerAliveCountMax = 3;
+        HashKnownHosts = false;
+        UserKnownHostsFile = "~/.ssh/known_hosts";
+        ControlMaster = "no";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "no";
       };
     };
   };
