175fdb4279e2102761ca1afbe7825e0308d91f79
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-08-07 15:34:45 -0700

    Some slight noctalia configuratino

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -247,6 +247,19 @@
     };
     programs.noctalia = {
       enable = true;
+
+      settings = { # This may also be a string or path to a .toml file.
+        theme = {
+          mode = "dark";
+          source = "builtin";
+          builtin = "Catppuccin";
+        };
+
+        # wallpaper = {
+          # enabled = true;
+          # default.path = "/path/to/wallpapers/wallpaper.png";
+        # };
+      };
     };
   };
 }
