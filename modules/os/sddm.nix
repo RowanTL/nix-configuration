99a70655e3f6f280d44399bfb32fa332791a1e78
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-08-12 23:54:03 -0700

    Add sddm theme

--- modules/os/sddm.nix
+++ modules/os/sddm.nix
@@ -1,5 +1,34 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 
+let
+  infinitySddmTheme = pkgs.stdenvNoCC.mkDerivation {
+    pname = "infinity-sddm-theme";
+    version = "unstable-2026-02-23";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "L4ki";
+      repo = "Infinity-Plasma-Themes";
+      rev = "e40490e79decb2f76d8e30c737bf7065f3112715";
+      hash = "sha256-T2Y0EHrxNLzype41Sb9ohn5vNakLCuJa/triJrGVv9U=";
+    };
+
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/share/sddm/themes
+      cp -r "Infinity-SDDM/Infinity-SDDM-6" "$out/share/sddm/themes/infinity-sddm-6"
+      runHook postInstall
+    '';
+
+    meta = with lib; {
+      description = "Infinity SDDM theme for Plasma 6";
+      homepage = "https://github.com/L4ki/Infinity-Plasma-Themes";
+      license = licenses.gpl3Plus;
+      platforms = platforms.linux;
+    };
+  };
+in
 {
   options = {
     sddm.enable
@@ -10,7 +39,9 @@
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+      theme = "infinity-sddm-6";
     };
+    environment.systemPackages = [ infinitySddmTheme ];
     services.xserver.xkb = {
       layout = "us,us";
       variant = "colemak,";
