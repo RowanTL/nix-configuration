5de856dcbb0f922709d22ea9c068c5a07a768ae0
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-08-29 10:14:57 -0500

    Delete swayfx comment from sway files

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -41,7 +41,6 @@
         noctalia = lib.getExe config.programs.noctalia.package;
       in {
       enable = true;
-      # package = pkgs.swayfx;
       wrapperFeatures.gtk = true; # Fixes common issues with GTK 3 apps
       config = {
         modifier = mod;
--- modules/os/sway.nix
+++ modules/os/sway.nix
@@ -12,7 +12,6 @@
     services.gnome.gnome-keyring.enable = true;
     programs.sway = {
       enable = true;
-      # package = pkgs.swayfx;
     };
     environment.systemPackages = with pkgs; [
       pulseaudio
