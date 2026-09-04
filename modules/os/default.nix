2be357c1482456b726c908b276a22182edb1ac0d
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-09-04 15:16:45 -0500

    remove hyprland and winapps cachix settings from default.nix

--- modules/os/default.nix
+++ modules/os/default.nix
@@ -14,15 +14,10 @@
         "https://winapps.cachix.org/"
       ];
 
-      trusted-substituters = [
-        "https://hyprland.cachix.org"
-      ];
-
       extra-substituters = [ "https://noctalia.cachix.org" ];
 
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "winapps.cachix.org-1:HI82jWrXZsQRar/PChgIx1unmuEsiQMQq+zt05CD36g="
       ];
 
       extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
